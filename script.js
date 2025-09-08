c2bc74076efe6d3bb3f582ee8d140fc108ed6af4
Sega19760 <81445297+Sega19760@users.noreply.github.com>
2025-09-07 21:11:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-import { DiscordSDK } from "@discord/embedded-app-sdk";
 
-const discordSdk = new DiscordSDK("YOUR_CLIENT_ID");
-
-async function setup() {
-  await discordSdk.ready();
-  await discordSdk.commands.authenticate();
-  console.log("Activity running!");
-}
-
-setup();
